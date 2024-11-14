db2e99ac1dc5d1fb809c57bfa6d02d61e896325f
zaemon1251-hesty <hisakawa1251@gmail.com>
2024-11-14 16:48:59 +0900

    add spotting algos

--- scripts/scbi-v2/eval-baseline.sh
+++ scripts/scbi-v2/eval-baseline.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
-# 沈黙時間 5.58 ← 1秒以上の空白があるコメント集合における平均的な発話間隔
+# 沈黙時間 2.14 ← 平均的な発話間隔
 # label_prob = [0.82, 0.18]  # 全体のラベル割合分布
 
 path="./data"
 
-python src/main_v2.py --path $path --mean_silence_sec 5.58
+echo "timing_algo: mean"
+python src/main_v2.py --path $path --mean_silence_sec 2.14
 
 << COMMENTOUT
 test 集合での結果
-diff_average: 5.2200
-label_accuracy: 0.7027
+diff_average: 2.60957
+label_accuracy: 0.70294
 confusion matrix: [
- [46116 10028]
- [10462  2313]
+ [46129 10015]
+ [10458  2317]
 ]
-label_f1: 0.8182
+label_f1: 0.81839
 COMMENTOUT
+
+echo "timing_algo: lognorm"
+python src/main_v2.py --path $path --timing_algo lognorm --lognorm_params '{"shape": 2.2926, "loc": 0.0, "scale": 0.2688}'
+# diff_average: 4.7374
+
+echo "timing_algo: gamma"
+python src/main_v2.py --path $path --timing_algo gamma --gamma_params '{"shape": 0.3283, "loc": 0.0, "scale": 6.4844}'
+# diff_average: 3.2179
+
+echo "timing_algo: expon"
+python src/main_v2.py --path $path --timing_algo expon --expon_params '{"loc": 0.0, "scale": 2.1289}'
+# diff_average: 3.0265
--- src/main_v2.py
+++ src/main_v2.py
@@ -5,18 +5,31 @@ import json
 from typing import Callable
 from sklearn.metrics import confusion_matrix
 from tap import Tap
+from scipy.stats import lognorm, expon, gamma
 
 
 class MainV2Argument(Tap):
     path: str = "./Benchmarks/TemporallyAwarePooling/data"
     fps: int = 1
+    timing_algo: str = "constant"
     mean_silence_sec: float = (
         5.58  # 1秒以上の空白があるコメント集合における 平均的な発話間隔
     )
+    lognorm_params: dict = {"shape": 2.2926, "loc": 0.0, "scale": 0.2688}
+    gamma_params: dict = {"shape": 0.3283, "loc": 0.0, "scale": 6.4844}
+    expon_params: dict = {"loc": 0.0, "scale": 2.1289}
+    ignore_under_1sec: bool = False
+
+    def configure(self):
+        self.add_argument("--lognorm_params", type=json.loads, required=False)
+        self.add_argument("--gamma_params", type=json.loads, required=False)
+        self.add_argument("--expon_params", type=json.loads, required=False)
 
 
 if __name__ == "__main__":
     args = MainV2Argument().parse_args()
+
+    print(f"{args.as_dict()=}")
 
     dataset_Test = CommentaryClipsForDiffEstimation(
         path=args.path,
@@ -28,11 +41,26 @@ if __name__ == "__main__":
 
     def predict_diff_and_label(previous_t):
         mean_silence_sec = args.mean_silence_sec
-        fps = args.fps
         label_space = [0, 1]  # 映像の説明, 付加的情報
         label_prob = [0.82, 0.18]  # 全体のラベル割合分布
 
-        next_t = previous_t + int(mean_silence_sec * fps)
+        if args.timing_algo == "constant":
+            next_t = previous_t + mean_silence_sec
+        elif args.timing_algo == "lognorm":
+            next_t = previous_t + lognorm.rvs(
+                s=args.lognorm_params["shape"],
+                scale=args.lognorm_params["scale"],
+            )
+        elif args.timing_algo == "gamma":
+            next_t = previous_t + gamma.rvs(
+                args.gamma_params["shape"],
+                scale=args.gamma_params["scale"],
+            )
+        elif args.timing_algo == "expon":
+            next_t = previous_t + expon.rvs(scale=args.expon_params["scale"])
+        else:
+            raise ValueError("Invalid timing_algo")
+
         next_label = np.random.choice(label_space, p=label_prob)
         return (next_t, next_label)
 
@@ -44,9 +72,13 @@ if __name__ == "__main__":
             "target_label": [],
         }
         for previous_frameid, target_frameid, target_label in dataset:
+            if args.ignore_under_1sec and (target_frameid - previous_frameid < 1):
+                continue
+
             next_frameid, predict_label = predict_model(previous_frameid)
             diff = abs(next_frameid - target_frameid)
             label_result = 1 if predict_label == target_label else 0
+
             result_dict["diff"].append(int(diff))
             result_dict["label_same"].append(int(label_result))
             result_dict["predict_label"].append(int(predict_label))
